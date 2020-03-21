c94bb6ba6389df56cafc162e5c233171f151de6a
jiechenmc <jiechenmc@gmail.com>
2020-03-21 11:09:03 -0400

    Fixed!

--- Module2/M2Project/M2PRO_Person_JieChen_File.py
+++ Module2/M2Project/M2PRO_Person_JieChen_File.py
@@ -4,12 +4,12 @@
 
 def write_to_file(people, file):
     with open(file, "w") as f:
-        f.write(
-            "FirstName\tLastName\t\t\t\tEmail\t\t\t\tPosition\t\tFull/PartTime\t\t\tSalary\n")
+        headers = ["FirstName".ljust(12), "LastName".ljust(12),
+                   "Email".ljust(25), "Position".ljust(12), "Full/PartTime", "Salary".rjust(10), "\n"]
+        f.writelines(headers)
         for person in people:
-            f.write(f"{person.get_firstName():<12}{person.get_lastName()}\
-                {person.get_email():<5}\t{person.get_position()}\
-                {person.get_full_part_time():<15}{person.get_salary()}\n")
+            f.write(
+                f"{person.get_firstName():<12}{person.get_lastName():<12}{person.get_email():<25}{person.get_position():<12}{person.get_full_part_time():<12}{person.get_salary():>10}\n")
 
 
 def read_file(file):
@@ -23,7 +23,7 @@ def read_file(file):
 
 if __name__ == "__main__":
     from M2PRO_Person_JieChen_Class import Employee
-    emp_1 = Employee("Jie", "Chen", "Manager", "Full", "100")
+    emp_1 = Employee("Jie", "Chen", "Manager", "Full", "100000000")
     emp_2 = Employee("Jenny", "Guo", "Waiter", "Part", "100")
     write_to_file([emp_1, emp_2], "employee.txt")
 
