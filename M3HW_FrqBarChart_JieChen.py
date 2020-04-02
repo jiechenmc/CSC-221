7e4c5868133f8eb1a371b76c47bb36b87e50bdbb
jiechenmc <jiechenmc@gmail.com>
2020-04-01 20:05:52 -0400

    Change

--- M3HW_FrqBarChart_JieChen.py
+++ M3HW_FrqBarChart_JieChen.py
@@ -3,15 +3,20 @@
 # CSC221 M3HW – FrqBarChart
 # Jie Chen
 
+# Part 1
 from textblob import TextBlob
 from nltk.corpus import stopwords
 from pathlib import Path
 from operator import itemgetter
 import pandas as pd
 
+# Part 2
+import imageio
+from wordcloud import WordCloud
+
 # Creating the blob
 blob = TextBlob(
-    Path(r"C:\Users\jiech\Documents\GitHub\CSC-221\Module3\RomeoAndJuliet.txt").read_text(encoding="utf-8"))
+    Path(r"RomeoAndJuliet.txt").read_text(encoding="utf-8"))
 
 # Removing Stop words
 stop_words = stopwords.words("english")
@@ -25,3 +30,5 @@ top20 = sorted_items[1:21]
 # Plotting the data
 df = pd.DataFrame(top20, columns=['word', 'count'])
 axes = df.plot.bar(x='word', y='count', legend=False)
+
+# Part 2
